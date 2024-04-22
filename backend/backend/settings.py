001fcccb81697a76ee3c823b2ab4f2591c1a9034
Matthew Li <mli007@hawaiiantel.net>
2024-04-22 14:57:36 -0400

    update on django.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ def getPath():
     #return Path(r'/mnt/c/Users/mli00/Desktop/Purdue/ECE 49595O/Boilermate-b3fcd-firebase-adminsdk-rwh4i-30e3b04f5c.json') # subject to change
     # return Path(r'C:\Users\andre\Documents\boilermate.json') # subject to change
     BOILERMATE_KEY = os.getenv("FIREBASE_API_KEY")
+    print(BOILERMATE_KEY)
     return Path(BOILERMATE_KEY)
 
 path = getPath()
